0f899cd02f9e97932f90600059850b4a4240d442
pasabaa <sanchez.ballesteros.pablo@gmail.com>
2022-12-20 12:18:40 -0600

    add loading while generatin image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from "openai"
 import { useState } from "react";
 import { InputBox } from "./components/InputBox";
+import LoadIcon from './assets/loading.svg'
 
 const configuration = new Configuration({
   apiKey: import.meta.env.VITE_API_KEY,
@@ -11,9 +12,10 @@ const openai = new OpenAIApi(configuration);
 function App() {
 
   const [userPrompt, setUserPrompt] = useState('');
-  const [number, setNumber] = useState('');
+  const [number, setNumber] = useState('1');
   const [size, setSize] = useState('256x256');
   const [imageUrl, setImageUrl] = useState('');
+  const [loading, setLoading] = useState(false);
 
   const generateImage = async () => {
 
@@ -23,20 +25,33 @@ function App() {
       size: size,
     }
 
-    const res = await openai.createImage(imageParams);
+    setLoading(true);
 
+    const res = await openai.createImage(imageParams);
+    
     const urlData = res.data.data[0].url;
     
     setImageUrl(urlData);
+
+    setLoading(false);
+
   }
 
+
   return (
-    <div >
-      {imageUrl && <img src={imageUrl} alt={'image openai'} />}
-      <InputBox label={'Description'} setAttribute={setUserPrompt} />
-      <InputBox label={'Amount'} setAttribute={setNumber} />
-      <InputBox label={'Size'} setAttribute={setSize} />
-      <button onClick={generateImage}>Generate</button>
+    <div className="grid place-items-center h-screen p-16" >
+      <div className="w-4/12 mx-auto sm:w-full">
+        {loading && <div className="h-full flex flex-col gap-4 items-center justify-center animate-pulse">
+          <img className="w-16" src={LoadIcon} alt="Loading Icon" />
+          <h1>Loading...</h1>
+        </div>}
+        {imageUrl && <img src={imageUrl} alt={'image openai'} />}
+        <InputBox label={'Description'} setAttribute={setUserPrompt} description={'A text description of the desired image(s). The maximum length is 1000 characters.'} />
+        <InputBox label={'Amount'} setAttribute={setNumber} description={'The number of images to generate. Must be between 1 and 10.'} />
+        <InputBox label={'Size'} setAttribute={setSize} description={'The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024.'} />
+        <button className="py-2 px-3 bg-gray-50 text-zinc-500 rounded uppercase font-bold text-sm hover:bg-gray-100 transition mb-8" onClick={generateImage}>Generate</button>
+      </div>
+
     </div>
   )
 }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
-export const InputBox = ({label, setAttribute}) => {
+export const InputBox = ({label, setAttribute, description}) => {
   return (
-    <div>
-        <label htmlFor={label}>{label}</label>
-        <input type="text" onChange={(e)=>setAttribute(e.target.value)} />
+    <div className='flex flex-col gap-2 my-4'>
+        <div className='flex flex-col leading-relaxed'>
+          <label className='text-gray-600 font-bold' htmlFor={label}>{label}</label>
+          <p className='text-sm text-gray-400'>{description}</p>
+        </div>
+        <input className='border rounded py-1 px-3 text-sm text-gray-600 focus:outline-none active:outline-none' onChange={(e)=>setAttribute(e.target.value)} />
     </div>
   )
 }
